1e1da6e690ab42f71aa53a47352800e196b16d67
janisur <janis.urbasts@gmail.com>
2020-06-25 10:31:50 +0300

    DAY3-Activities-PART1

--- SEF/src/sef/module6/activity/Employee_I.java
+++ SEF/src/sef/module6/activity/Employee_I.java
@@ -1,7 +1,7 @@
 package sef.module6.activity;
 
 // class Employee_I must extend class Person_I. Complete code for the same
-public class Employee_I  {
+public class Employee_I extends Person_I {
 	
 	//Attributes
 	private double salary;
@@ -9,16 +9,32 @@ public class Employee_I  {
 	
 	//Behavior - write default constructor. Print 'I'm an Employee_I Constructor'
 	
-	
+	public Employee_I(){ 
+		System.out.println("I'm an Employee_I Constructor"); 
+	}
+		
 	// write getter for int salary
 	
+	public double getSalary() {
+		return salary;
+	}
 	
 	// write setter for int salary
 	
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
 	
 	// write getter for String title
 	
+	public String getTitle() {
+		return title;
+	}
 	
 	// write setter for String title
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
 	
 }
